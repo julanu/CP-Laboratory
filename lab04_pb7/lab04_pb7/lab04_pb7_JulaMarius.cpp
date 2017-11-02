3f6339e83daf86acc6d4e79763d1758e3243cd99
julanu <marius.jula@yahoo.com>
2017-11-02 16:35:07 +0200

    up

--- lab04_pb7/lab04_pb7/lab04_pb7_JulaMarius.cpp
+++ lab04_pb7/lab04_pb7/lab04_pb7_JulaMarius.cpp
@@ -5,31 +5,65 @@
 #include <stdio.h>
 #include <conio.h>
 
-float input();
-
+int   input_int();
+float input_real();
+float compute_real(float, float, float);
+double compute_int(int, int, int);
 
 void main()
 {
-	float a, b, c, sum;
+	float a, b, c;
+	int x, y, z;
 
-	sum = 0.0;
+	printf("\nPlease enter the 3 real numbers: ");
+	a = input_real();
+	b = input_real(); 
+	c = input_real();
+	printf("The value is: %.2f", compute_real(a, b, c));
 
-	printf("\nPlease enter the 3 numbers: ");
-	a = input();
-	b = input(); 
-	c = input();
 
-	sum = 1. / a + 1. / b + 1. / c;
-
-	printf("The value is: %.2f", sum);
+	printf("\nPlease enter the 3 integer numbers: ");
+	x = input_int();
+	y = input_int();
+	z = input_int();
+	printf("The value is: %.2f", compute_int(x, y, z));
 
 	_getch();
 
 }
 
-float input()
+float input_real()
 {
 	float x;
+
 	scanf("%f", &x);
+
 	return x;
+}
+
+int input_int()
+{
+	int x;
+
+	scanf("%d", &x);
+
+	return x;
+}
+
+float compute_real(float a, float b, float c)
+{
+	float sum = 0.0;
+
+	sum = 1 / a + 1 / b + 1 / c;
+
+	return sum;
+}
+
+double compute_int(int x, int y, int z)
+{
+	double sum = 0.0;
+
+	sum = 1. / x + 1. / y + 1. / z;
+
+	return sum;
 }
